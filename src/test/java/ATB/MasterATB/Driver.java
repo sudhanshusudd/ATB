d25e0cfc31de044714c79cd503aabea39beb1f32
sudhanshusudd <sudhanshu.chaurasia.sel@gmail.com>
2022-09-04 22:33:13 +0530

    Add files via upload

---
+++ src/test/java/ATB/MasterATB/Driver.java
@@ -0,0 +1,18 @@
+package ATB.MasterATB;
+
+import org.testng.annotations.*;
+
+public class Driver {
+	
+	@BeforeTest
+	public void preRequisites() {
+		System.out.println("running pre requisites..");
+	}
+	
+	@Test
+	public void Test() {
+		
+	System.out.println("Test in progress..");
+	}
+
+}
