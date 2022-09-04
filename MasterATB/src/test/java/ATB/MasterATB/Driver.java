6dfdd17b809a41cd442bec77fe5c5baac47511bf
sudhanshusudd <sudhanshu.chaurasia.sel@gmail.com>
2022-09-04 22:17:17 +0530

    Update Driver.java

--- MasterATB/src/test/java/ATB/MasterATB/Driver.java
+++ MasterATB/src/test/java/ATB/MasterATB/Driver.java
@@ -4,10 +4,13 @@ import org.testng.annotations.*;
 
 public class Driver {
 	
-	@Test
-	public void Test() {
-		
-	System.out.println("Test in progress..");
+	@BeforeTest
+	public void preRequisites() {
+		System.out.println("running pre requisites..");
 	}
-
+	
+	@Test
+	public void Test() {		
+		System.out.println("Test in progress..");
+	}
 }
